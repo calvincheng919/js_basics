f030a3391a3444706a03b20e15ebace974cb35a5
Calvin Cheng <calvincheng919@gmail.com>
2021-02-10 07:59:41 -0800

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,19 @@
+// 1. declare a variable named myNumber and assign it the number 5
+
+
+// 2. increment myNumber by 1 
+
+
+// 3. console log muNumber
+
+
+// 4. find the remainder of 5 divided by 2 and console log the result
+
+
+// 5. console log - This is Peter's book
+
+
+// 6. console log - Alan said, "Peter is learning JavaScript" 
+
+
+// 7. Find the length of the string JavaScript and console log the result
