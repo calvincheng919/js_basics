af120e4e3d0b54f7295b5c46105fd16137f53935
Calvin Cheng <calvincheng919@gmail.com>
2021-02-16 07:38:24 -0800

    js functions

--- basics.js
+++ basics.js
@@ -1,19 +1,48 @@
-// 1. declare a variable named myNumber and assign it the number 5
+// create an index.html stub and connect basics.js to that index.html.
 
+// proof of life - console log "hello world"
+
+console.log('hello world');
+
+// 1. declare a variable named myNumber and assign it the number 5
+// then console log myNumber
+
+var myNumber = 5;
+console.log(myNumber);
 
 // 2. increment myNumber by 1 
 
+//PAY ATTENTION
 
-// 3. console log muNumber
+// myNumber = myNumber++; 
+myNumber = myNumber + 1; 
+console.log(myNumber); //6
+
+// 3. console log myNumber
+// myNumber should be 6
 
 
 // 4. find the remainder of 5 divided by 2 and console log the result
+// 1 is the answer. use %
 
+console.log(5%2); //give you 1
 
 // 5. console log - This is Peter's book
 
+// how to escape characters in javascript
+console.log('This is Peter\'s book');
+
+//What is "escape"?
+// when you escape, you are telling JavaScript interpreter to ignore the character.
 
 // 6. console log - Alan said, "Peter is learning JavaScript" 
 
+console.log('Alan said, \"Peter is learning JavaScript\"');
 
-// 7. Find the length of the string JavaScript and console log the result
+// 7. Find the length of the string "JavaScript" and console log the result
+
+var myString = 'JavaScript';
+
+console.log(myString.length); //expect 10
+
+
